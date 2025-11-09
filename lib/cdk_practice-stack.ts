d6ac6b70a7ae252266b63abbea758b2eee01e89d
ShayneSL1 <ssluangvisa@gmail.com>
2025-11-09 02:21:06 -0500

    Initial building an s3 bucket using the interface made prior, within the class CdkPracticeStack using the constructor-super template.

--- lib/cdk_practice-stack.ts
+++ lib/cdk_practice-stack.ts
@@ -1,16 +1,24 @@
+import { S3 } from 'aws-cdk-lib/aws-ses-actions';
 import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
+interface MyBucketProps {
+    bucketName: string;
+    versioned: boolean;
+    encryption: S3.BucketEncryption;
+}
+
 export class CdkPracticeStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    
+    const bucketProps: MyBucketProps = {
+      bucketName: "myS3-Bucket-SS-CDK" + this.account,
+      versioned: true,
+      encryption: S3.BucketEncryption.S3_MANAGED;
+    };
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkPracticeQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new S3.bucket(this, "myFirstBucket", bucketProps)
   }
 }
